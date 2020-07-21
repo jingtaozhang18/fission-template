34299483d626a6f99c10586ffe3630c9c3609150
zhangjingtao <851207685@qq.com>
2020-07-21 09:12:11 +0800

    添加使用说明 & 修改拼写错误

--- {{cookiecutter.item_name}}/src/main.py
+++ {{cookiecutter.item_name}}/src/main.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from flask import request as flask_request
 import json
+from flask import g
+
 
 def main():
+    #  打日志
+    # g.logger.info()
+
+    #  打点
+    # metric name 为埋点的名称
+    # labels 为埋点的标签信息，为字典结构
+    # g.metric_handler.counter("metric name", labels, 1)  # 通常用于统计频率信息等
+    # g.metric_handler.gauge("metric name", labels, 1)  # 通常用于持续计数等
+
     return json.dumps(flask_request.json)
